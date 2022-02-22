7ce0bc15c91a5d3c7943029d240bdff8d511aaad
Vlad Arsenjtev <Vlad.arsenjtev@gmail.com>
2022-02-22 16:44:47 +0200

    initial commit

--- src/com/vladarsenjtev/Main.java
+++ src/com/vladarsenjtev/Main.java
@@ -3,6 +3,10 @@ package com.vladarsenjtev;
 public class Main {
 
     public static void main(String[] args) {
+        Person person1 = new Person("Svituxov", "Dmitro", "Olegovich");
+        Person person2 = new Person("Arsenjtev Vlad Igorovich");
 
+        System.out.println(person1);
+        System.out.println(person2);
     }
 }
